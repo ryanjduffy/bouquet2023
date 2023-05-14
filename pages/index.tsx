384e0425aa973dc56badb4711272ac8dc701e6ad
Jon Bell <github@lot23.com>
2023-05-14 19:46:15 +1200

    Small logo thing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,23 @@
+import React from "react";
 import Head from "next/head";
-import { GetServerSideProps } from "next";
-import styles from "../styles/bouquet.module.css";
 
-interface Emoji {
-  id: number;
-  emoji: string;
-}
-
-interface HomeProps {
-  emojis: Emoji[];
-}
-
-export default function Home({ emojis }: HomeProps): JSX.Element {
+export default function Home(): JSX.Element {
   return (
     <>
       <Head>
         <title>bouquet</title>
-        <meta name="description" content="All emojis from the database" />
+        <meta name="description" content="Coming Soon" />
       </Head>
-      <div className="bg-black text-white p-3 text-center">
-        RSS only for now
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+          background: "#EFF3E4",
+        }}
+      >
+        <img src="/bouquet.png" alt="bouquet" className="h-32" />
       </div>
     </>
   );
