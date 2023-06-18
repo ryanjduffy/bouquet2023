ba6c2a32dbb83e284cbb1493cb4403104df00a7c
Jon Bell <github@lot23.com>
2023-06-18 17:12:02 +1200

    Fixing cookies

--- cypress/e2e/1-basic/cookies.cy.js
+++ cypress/e2e/1-basic/cookies.cy.js
@@ -1,64 +1,43 @@
-it('cy.setCookie() - set a browser cookie', () => {
-
+it("cy.setCookie() - set a browser cookie", () => {
   // https://on.cypress.io/setcookie
 
-  cy.getCookies().should('be.empty')
+  cy.getCookies().should("be.empty");
 
-
-  cy.setCookie('foo', 'bar')
-
+  cy.setCookie("foo", "bar");
 
   // cy.getCookie() yields a cookie object
 
-  cy.getCookie('foo').should('have.property', 'value', 'bar')
+  cy.getCookie("foo").should("have.property", "value", "bar");
+});
 
-})
-
-
-it('cy.clearCookie() - clear a browser cookie', () => {
-
+it("cy.clearCookie() - clear a browser cookie", () => {
   // https://on.cypress.io/clearcookie
 
-  cy.getCookie('token').should('be.null')
+  cy.getCookie("token").should("be.null");
 
+  cy.get("#clearCookie .set-a-cookie").click();
 
-  cy.get('#clearCookie .set-a-cookie').click()
-
-
-  cy.getCookie('token').should('have.property', 'value', '123ABC')
-
+  cy.getCookie("token").should("have.property", "value", "123ABC");
 
   // cy.clearCookies() yields null
 
-  cy.clearCookie('token').should('be.null')
+  cy.clearCookie("token").should("be.null");
 
+  cy.getCookie("token").should("be.null");
+});
 
-  cy.getCookie('token').should('be.null')
-
-})
-
-
-it('cy.clearCookies() - clear browser cookies', () => {
-
+it("cy.clearCookies() - clear browser cookies", () => {
   // https://on.cypress.io/clearcookies
 
-  cy.getCookies().should('be.empty')
+  cy.getCookies().should("be.empty");
 
+  cy.get("#clearCookies .set-a-cookie").click();
 
-  cy.get('#clearCookies .set-a-cookie').click()
-
-
-  cy.getCookies().should('have.length', 1)
-
+  cy.getCookies().should("have.length", 1);
 
   // cy.clearCookies() yields null
 
-  cy.clearCookies()
+  cy.clearCookies();
 
-
-  cy.getCookies().should('be.empty')
-
-})
-
-})
-
+  cy.getCookies().should("be.empty");
+});
