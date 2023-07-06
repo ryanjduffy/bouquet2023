bbb936afa4a915164bfaa7ee6f6ab4f9158d18d1
Ryan Duffy <ryan@replay.io>
2023-07-05 17:29:58 -0700

    add baseUrl config

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,7 @@ import * as replay from '@replayio/cypress';
 
 export default defineConfig({
   e2e: {
+    baseUrl: 'https://bouquet.lot23.com',
     setupNodeEvents(on, config) {
       replay.default(on, config);
       return config;
